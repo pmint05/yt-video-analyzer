5d9013d03c41fbec30f5d0d7095fc9e86d3a5dff
Thong Pham <phucon547@gmail.com>
2025-08-03 12:42:47 +0700

    Update result page UI

--- src/public/scripts/main.js
+++ src/public/scripts/main.js
@@ -12,40 +12,42 @@ const YOUTUBE_URL_REGEX =
 	/^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/;
 const API_ENDPOINT = "/analyze";
 
-urlForm.addEventListener("submit", (event) => {
-	event.preventDefault();
+if (urlForm) {
+	urlForm.addEventListener("submit", (event) => {
+		event.preventDefault();
 
-	const url = urlInput.value.trim();
+		const url = urlInput.value.trim();
 
-	if (!YOUTUBE_URL_REGEX.test(url)) {
-		alert("Please enter a valid YouTube URL.");
-		console.error("Invalid YouTube URL.");
-		return;
-	}
+		if (!YOUTUBE_URL_REGEX.test(url)) {
+			alert("Please enter a valid YouTube URL.");
+			console.error("Invalid YouTube URL.");
+			return;
+		}
 
-	resultContainer.classList.remove("show");
-	message.classList = [];
+		resultContainer.classList.remove("show");
+		message.classList = [];
 
-	fetch(API_ENDPOINT, {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({ url: url }),
-	})
-		.then((res) => res.json())
-		.then((data) => {
-			console.log("Response:", data);
-			if (data.success) {
-				const { jobId } = data;
-				resultJobId.textContent = jobId;
-				resultUrl.href = `/result/${jobId}`;
-				resultContainer.classList.add("show");
-			} else {
-				console.error("Error:", data.error);
-				message.textContent = "An error occurred: " + data.error;
-				message.classList.add("error");
-			}
-		});
-	// urlInput.value = ""; // Clear the input after submission
-});
+		fetch(API_ENDPOINT, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ url: url }),
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				console.log("Response:", data);
+				if (data.success) {
+					const { jobId } = data;
+					resultJobId.textContent = jobId;
+					resultUrl.href = `/result/${jobId}`;
+					resultContainer.classList.add("show");
+				} else {
+					console.error("Error:", data.error);
+					message.textContent = "An error occurred: " + data.error;
+					message.classList.add("error");
+				}
+			});
+		urlInput.value = ""; // Clear the input after submission
+	});
+}
